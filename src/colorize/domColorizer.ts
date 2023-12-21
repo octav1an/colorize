86786ff48434bede8d9eac5a7716ebf21d764bed
octav1an <octav1an@hotmail.com>
2023-12-21 16:53:36 +0100

    rename functions

--- src/colorize/domColorizer.ts
+++ src/colorize/domColorizer.ts
@@ -11,26 +11,33 @@ export class DomColorizer {
   public addColorToPaths(paths: Path[], colorSettings: ColorizeSettings) {
     for (const pathSetting of paths) {
       const colorValue = this.mapColorNameToColor(pathSetting, colorSettings);
-      this.addColorToPath(pathSetting.path, colorValue);
+      this.updateColorToPath(pathSetting.path, colorValue);
     }
   }
 
   /**
    * Changes the color of a file-explorer item
+   * To remove the color pass empty string ""
    * @param path Path of the file item to change the color for
    * @param color New background color
    */
-  public addColorToPath(path: string, color: string) {
+  public updateColorToPath(path: string, color: string) {
+    const fileExplorer = this.getFileExplorer();
+
+    const item = fileExplorer.view.fileItems[path].coverEl;
+    item.style.backgroundColor = color;
+  }
+
+  private getFileExplorer() {
     // The assumption is that there is only one file explorer leaf per vault
     const fileExplorer =
       this.plugin.app.workspace.getLeavesOfType("file-explorer")[0];
-    // console.log(fileExplorer);
+
     if (!fileExplorer) {
       throw new Error("No file-explorer leaf exists");
     }
 
-    const item = fileExplorer.view.fileItems[path].coverEl;
-    item.style.backgroundColor = color;
+    return fileExplorer;
   }
 
   /**
